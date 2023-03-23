64169001b87e9e65dd13970bfad565bdc0bdb8c0
MIM14 <imxym14@gmail.com>
2023-03-23 10:04:31 +0100

    Either implemented

---
+++ app/src/main/java/com/moaimar/ricknmortyapp/app/domain/ErrorApp.kt
@@ -0,0 +1,8 @@
+package com.moaimar.ricknmortyapp.app.domain
+
+sealed class ErrorApp {
+    object DataLayerError : ErrorApp()
+    object NoInternetError: ErrorApp()
+    object TimeOutError: ErrorApp()
+    object UnKnowError: ErrorApp()
+}
